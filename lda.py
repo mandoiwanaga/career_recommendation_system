febc00e24d8629b26657816d26ed2b63a1be27f4
Mando Iwanaga <mando_iwanaga08@hotmail.com>
2019-07-11 09:42:54 -0700

    remove unused function

--- lda.py
+++ lda.py
@@ -5,35 +5,6 @@ from gensim.corpora.dictionary import Dictionary
 
 
 
-def compute_coherence_scores(dictionary, corpus, texts, limit, start=2, step=3):
-    """
-    Compute c_v coherence for various number of topics
-
-    Parameters:
-    ----------
-    dictionary : Gensim dictionary
-    corpus : Gensim corpus
-    texts : List of input texts
-    limit : Max num of topics
-
-    Returns:
-    -------
-    model_list : List of LDA topic models
-    coherence_values : Coherence values corresponding to the LDA model with respective number of topics
-    """
-    coherence_values = []
-    model_list = []
-    for num_topics in range(start, limit, step):
-        model=gensim.models.LdaMulticore(corpus=corpus, id2word=dictionary, 
-                                         num_topics=num_topics, chunksize=100,
-                                        passes=20, workers=4)
-        model_list.append(model)
-        coherencemodel = CoherenceModel(model=model, texts=texts, dictionary=dictionary, coherence='c_v')
-        coherence_values.append(coherencemodel.get_coherence())
-
-    return model_list, coherence_values
-
-
 
 
 def show_topics_sentences(ldamodel, corpus, texts):
