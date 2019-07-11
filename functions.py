fb4b35e1e8393355564858efcfa70615d2cc3648
Mando Iwanaga <mando_iwanaga08@hotmail.com>
2019-07-10 20:20:52 -0700

    seperate cleaning functions from model functions

--- functions.py
+++ functions.py
@@ -1223,63 +1223,6 @@ def remove_stop_words(text):
 
 
 
-def compute_coherence_scores(dictionary, corpus, texts, limit, start=2, step=3):
-    """
-    Compute c_v coherence for various number of topics
-
-    Parameters:
-    ----------
-    dictionary : Gensim dictionary
-    corpus : Gensim corpus
-    texts : List of input texts
-    limit : Max num of topics
-
-    Returns:
-    -------
-    model_list : List of LDA topic models
-    coherence_values : Coherence values corresponding to the LDA model with respective number of topics
-    """
-    coherence_values = []
-    model_list = []
-    for num_topics in range(start, limit, step):
-        model=gensim.models.LdaMulticore(corpus=corpus, id2word=dictionary, 
-                                         num_topics=num_topics, chunksize=100,
-                                        passes=20, workers=4)
-        model_list.append(model)
-        coherencemodel = CoherenceModel(model=model, texts=texts, dictionary=dictionary, coherence='c_v')
-        coherence_values.append(coherencemodel.get_coherence())
-
-    return model_list, coherence_values
-
-
-
-
-def show_topics_sentences(ldamodel, corpus, texts):
-    """Returns dataframe with topics and descriptions"""
-    sent_topics_df = pd.DataFrame()
-
-    # Get main topic in each document
-    for i, row in enumerate(ldamodel[corpus]):
-        row = sorted(row, key=lambda x: (x[1]), reverse=True)
-        # Get the Dominant topic, Perc Contribution and Keywords for each document
-        for j, (topic_num, prop_topic) in enumerate(row):
-            if j == 0:  # => dominant topic
-                wp = ldamodel.show_topic(topic_num)
-                topic_keywords = ", ".join([word for word, prop in wp])
-                sent_topics_df = sent_topics_df.append(pd.Series([int(topic_num), 
-                                                                  round(prop_topic,4), 
-                                                                  topic_keywords]), 
-                                                                   ignore_index=True)
-            else:
-                break
-    sent_topics_df.columns = ['Dominant_Topic', 'Perc_Contribution', 'Topic_Keywords']
-
-    # Add original text to the end of the output
-    contents = pd.Series(texts)
-    sent_topics_df = pd.concat([sent_topics_df, contents], axis=1)
-    return(sent_topics_df)
-
-
 
 
 
